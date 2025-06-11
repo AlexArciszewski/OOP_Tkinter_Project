685caaa65736f3b00b1af36eed719899230bc354
AlexArciszewski <arciszew@yahoo.com>
2025-06-11 18:47:07 +0200

    Fixing bugs adding pep 8 layout

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-from typing import Tuple, Any, List
+from typing import Optional
 from dotenv import load_dotenv
 import requests
 import smtplib
 import os
 
+
+
 load_dotenv()
 
-EMAIL = "aaleksandro123@gmail.com"
+MY_EMAIL = os.getenv("MY_EMAIL")
 PASSWORD = os.getenv("EMAIL_PASSWORD")
 
 
 class IssNotifier:
     """Creates an OBJ class of ISS notifier"""
+    
     def __init__(self, my_lat:float, my_long:float, email:str, password:str):
         self.my_lat = my_lat
         self.my_long = my_long
         self.email = email
         self.password = password
 
-        
+
+
 def main() -> None:
      notifier = IssNotifier(my_lat=52.248421, my_long=21.172608,
-        email="aaleksandro123@gmail.com",
+        email=MY_EMAIL,
         password=PASSWORD 
      )        
         
